24455d2f967012a9a1c8e60eafb239583b365e64
Cyriac <drevia59@gmail.com>
2023-03-17 15:20:13 +0100

    add spring boot projet

---
+++ project5/src/main/java/com/openclassroom/project5/Project5SafenetApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroom.project5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project5SafenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project5SafenetApplication.class, args);
+	}
+
+}
